7672819b30f1720614b1627305f4e9ce23a11151
hcoffey1 <hcoffey1@vols.utk.edu>
2018-09-04 15:47:41 -0400

    Added super secret file

---
+++ t2.py
@@ -0,0 +1,12 @@
+import tkinter as tk
+
+root = tk.Tk()
+logo = tk.PhotoImage(file="~/Pictures/supremeLord.gif")
+
+w1 = tk.Label(root, image=logo).pack(side="right")
+
+
+
+w2 = tk.Label(root, justify=tk.LEFT, padx = 10,
+			text="Hello Tkinter!").pack(side="left")
+root.mainloop()
