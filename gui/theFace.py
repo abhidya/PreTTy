7c78cc561e1fcc41329428bce3ad25515c4907ed
hcoffey1 <hcoffey1@vols.utk.edu>
2018-09-13 16:19:05 -0400

    Added toggleable window for viewing directory

--- gui/theFace.py
+++ gui/theFace.py
@@ -12,6 +12,11 @@ def updateWindow(command=''):
 def get(event):
     updateWindow(event.widget.get())
 
+def hide_me():
+    output.pack_forget()
+def show_me():
+    output.pack(side=tk.LEFT)
+
 root = tk.Tk()
 
 root.title("preTTY")
@@ -20,8 +25,14 @@ root.configure(background="black")
 app_name = tk.Label(root, text="preTTY", bg="black", fg="white", font="none 12 bold")
 app_name.pack(side=tk.TOP)
 
-output = tk.Text(root, width=75, height=6, wrap=tk.WORD, background="white")
-output.pack(side=tk.BOTTOM)
+output = tk.Text(root, width=40, height=60, wrap=tk.WORD, bg="black", fg="white")
+#output.pack(side=tk.LEFT)
+
+file_toggle = tk.Button(root, text="file view", command=hide_me);
+file_toggle_show = tk.Button(root, text="file view-show", command=show_me);
+
+file_toggle.pack(side=tk.LEFT)
+file_toggle_show.pack(side=tk.LEFT)
 
 e = tk.Entry(root, width=25)
 e.bind('<Return>',get)
