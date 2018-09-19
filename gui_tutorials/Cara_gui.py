bc4e4d5bce622c01ef4e2b9d7187cb701db0604e
Cara Scott <cscott57@vols.utk.edu>
2018-09-19 18:00:16 -0400

    Please test this Part 2

--- gui_tutorials/Cara_gui.py
+++ gui_tutorials/Cara_gui.py
@@ -1,14 +1,35 @@
 import tkinter as tk
+from PIL import Image, ImageTk
 
-#app = tk.Tk()
+# --- functions ---
+
+# --- main ---
 
 root = tk.Tk()
-b = tk.Button(root,justify = LEFT)
-photo = tk.PhotoImage(file="BadHombre.png")
-b.config(image=photo,width="100",height="100")
+
+# load image
+image = Image.open("../gui/images/apple.gif")
+photo = ImageTk.PhotoImage(image)
+
+# label with image
+l = tk.Label(root, image=photo)
+l.pack()
+
+# bind click event to image
+l.bind('<Button-1>', on_click)
+
+# button with image binded to the same function 
+b = tk.Button(root, image=photo, command=on_click)
+b.pack()
+
+# button with text closing window
+b = tk.Button(root, text="Close", command=root.destroy)
+b.pack()
+
+#b = tk.Button(root)
+#photo = tk.PhotoImage(file="BadHombre.png")
+#b.config(image=photo,width="100",height="100")
 #b.pack(side=LEFT)
-
-
 
 root.geometry("1000x725")
 root.mainloop()
