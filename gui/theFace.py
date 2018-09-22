e8b4963327f117c12e5c4883203735fa28dec65a
hcoffey1 <hcoffey1@vols.utk.edu>
2018-09-22 19:30:02 -0400

    Changed GUI to class based implementation

--- gui/theFace.py
+++ gui/theFace.py
@@ -1,58 +1,63 @@
 import tkinter as tk
 import subprocess
 
-
-#TODO: Change to class based implementation
 #TODO: Decide whether to use grid or pack approach for labels
-
-#command is a string retrieved from text box
-def updateWindow(command=''):
-    output.config(state=tk.NORMAL)
-    output.delete(0.0,tk.END)
-    p = subprocess.Popen(["ls", "-al"], stdout=subprocess.PIPE)
-    current_dir = p.communicate()[0]
-    output.insert(tk.END, current_dir)
-    output.config(state=tk.DISABLED)
+class app(object):
+    def __init__(self, parent):
+        self.root = parent
+        self.root.title("preTTY")
+        self.root.geometry("800x500")
+        self.root.configure(background="black")
 
 
-def get(event):
-    updateWindow(event.widget.get())
+        #Application title displayed on window
+        self.app_name = tk.Label(self.root, text="preTTY", bg="black", fg="white", font="none 30 bold")
+        self.app_name.pack(side=tk.TOP)
 
-#hides the lefthand text box
-def hide_me():
-    output.pack_forget()
+        #Box to display current dirctory
+        #TODO: Change this to be interactive
+        self.output = tk.Text(self.root, width=40, height=60, wrap=tk.WORD, bg="black", fg="white")
+        
+        #Buttons to hide and show text display
+        #TODO: Change to one button that toggles display
+        file_toggle = tk.Button(self.root, text="file view", command=self.hide_me);
+        file_toggle_show = tk.Button(self.root, text="file view-show", command=self.show_me);
 
-#shows the lefthand text box
-def show_me():
-    output.pack(side=tk.LEFT)
+        file_toggle.pack(side=tk.BOTTOM)
+        file_toggle_show.pack(side=tk.BOTTOM)
 
-root = tk.Tk()
-root.geometry("800x500")
-root.title("preTTY")
-root.configure(background="black")
+        #Command prompt
+        #TODO: Add autofocus when app is first accessed
+        #TODO: Add hotkey to set focus easily
+        e = tk.Entry(self.root, width=25)
+        e.bind('<Return>',self.get)
+        e.pack(side=tk.BOTTOM)
 
-#Application title displayed on window
-app_name = tk.Label(root, text="preTTY", bg="black", fg="white", font="none 30 bold")
-app_name.pack(side=tk.TOP)
+        self.updateWindow()
 
-#Box to display current dirctory
-#TODO: Change this to be interactive
-output = tk.Text(root, width=40, height=60, wrap=tk.WORD, bg="black", fg="white")
+    #command is a string retrieved from text box
+    def updateWindow(self, command=''):
+        self.output.config(state=tk.NORMAL)
+        self.output.delete(0.0,tk.END)
+        p = subprocess.Popen(["ls", "-al"], stdout=subprocess.PIPE)
+        current_dir = p.communicate()[0]
+        self.output.insert(tk.END, current_dir)
+        self.output.config(state=tk.DISABLED)
 
-#Buttons to hide and show text display
-#TODO: Change to one button that toggles display
-file_toggle = tk.Button(root, text="file view", command=hide_me);
-file_toggle_show = tk.Button(root, text="file view-show", command=show_me);
 
-file_toggle.pack(side=tk.BOTTOM)
-file_toggle_show.pack(side=tk.BOTTOM)
+    def get(self, event):
+        updateWindow(event.widget.get())
 
-#Command prompt
-#TODO: Add autofocus when app is first accessed
-#TODO: Add hotkey to set focus easily
-e = tk.Entry(root, width=25)
-e.bind('<Return>',get)
-e.pack(side=tk.BOTTOM)
+    #hides the lefthand text box
+    def hide_me(self):
+        self.output.pack_forget()
 
-updateWindow()
-root.mainloop()
+    #shows the lefthand text box
+    def show_me(self):
+        self.output.pack(side=tk.LEFT)
+
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    gui = app(root)
+    root.mainloop()
