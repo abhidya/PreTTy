53ce39c9bd105bd4957a7a8e02e9437845d995fc
Cara Scott <cscott57@vols.utk.edu>
2018-09-22 10:55:22 -0400

    Changes picture/button size

--- gui_tutorials/Cara_gui.py
+++ gui_tutorials/Cara_gui.py
@@ -1,14 +1,26 @@
 import tkinter as tk
+import PIL
 from PIL import Image, ImageTk
 
 # --- functions ---
+
+def on_click(event=None):
+    # `command=` calls function without argument
+    # `bind` calls function with one argument
+    print("image clicked")
 
 # --- main ---
 
 root = tk.Tk()
 
-# load image
-image = Image.open("../gui/images/apple.gif")
+# load image and resize it
+basewidth = 200
+image = Image.open("../gui/images/alpaca.gif")
+#photo = ImageTk.PhotoImage(image)
+wpercent = (basewidth / float(image.size[0]))
+hsize = int((float(image.size[1]) * float(wpercent)))
+image = image.resize((basewidth, hsize), PIL.Image.ANTIALIAS)
+image.save("resized_image.gif") 
 photo = ImageTk.PhotoImage(image)
 
 # label with image
@@ -23,13 +35,9 @@ b = tk.Button(root, image=photo, command=on_click)
 b.pack()
 
 # button with text closing window
-b = tk.Button(root, text="Close", command=root.destroy)
+b = tk.Button(root, text="Close", fg="red", activeforeground="blue", background="cyan", command=root.destroy)
 b.pack()
+#bg="#ff340a"
 
-#b = tk.Button(root)
-#photo = tk.PhotoImage(file="BadHombre.png")
-#b.config(image=photo,width="100",height="100")
-#b.pack(side=LEFT)
-
-root.geometry("1000x725")
+root.geometry("500x300")
 root.mainloop()
