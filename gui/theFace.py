cf8cd725b360e48d59037ab8a50e4b48394efc59
hcoffey1 <hcoffey1@vols.utk.edu>
2018-09-22 20:32:38 -0400

    Begun frame implementation, and partially optimized output toggle

--- gui/theFace.py
+++ gui/theFace.py
@@ -9,15 +9,17 @@ class app(object):
     def __init__(self, parent):
         self.root = parent
         self.root.title("preTTY")
-#        self.root.geometry("800x500")
+        self.root.geometry("800x500")
         self.root.configure(background="black")
         self.root.bind_all("<Control-q>", self.quit)
+
+        self.prompt_frame = tk.Frame(self.root, bg="")
+        self.prompt_frame.pack(side=tk.BOTTOM)
 
 
         #Application title displayed on window
         self.app_name = tk.Label(self.root, text="preTTY", bg="black", fg="white", font="none 30 bold")
-        #self.app_name.pack(side=tk.TOP)
-        self.app_name.grid(row=0, column=2)
+        self.app_name.pack(side=tk.TOP)
 
         #Box to display current dirctory
         #TODO: Change this to be interactive
@@ -25,19 +27,21 @@ class app(object):
         
         #Buttons to hide and show text display
         #TODO: Change to one button that toggles display
-        self.output_hide_button = tk.Button(self.root, text="file view", command=self.hide_me);
-        self.output_display_button = tk.Button(self.root, text="file view-show", command=self.show_me);
+        self.output_hide_button = tk.Button(self.prompt_frame, text="file view", command=self.hide_me);
+        self.output_display_button = tk.Button(self.prompt_frame, text="file view-show", command=self.show_me);
 
+#        testframe = tk.Frame(self.root,bg="red", height=10, width=10)
+ #       testframe.grid(row=0,column=0)
         #Command prompt
         #TODO: Add hotkey to set focus easily
-        e = tk.Entry(self.root, width=25)
+        e = tk.Entry(self.prompt_frame, width=25)
         e.bind('<Return>',self.get)
         #e.pack(side=tk.BOTTOM)
-        e.grid(row=3, column=2)
+        e.grid(row=1, column=0)
         e.focus()
 
         #self.output_display_button.pack(side=tk.BOTTOM)
-        self.output_display_button.grid(row=2, column=2)
+        self.output_display_button.grid(row=0, column=0)
         self.updateWindow()
 
     #command is a string retrieved from text box
@@ -56,21 +60,15 @@ class app(object):
     #hides the lefthand text box
     def hide_me(self):
         pass
-        #self.output.pack_forget()
-        self.output.grid_forget()
-       # self.output_hide_button.pack_forget()
+        self.output.pack_forget()
         self.output_hide_button.grid_forget()
-       # self.output_display_button.pack(side=tk.BOTTOM)
-        self.output_display_button.grid(row=2,column=2)
+        self.output_display_button.grid(row=0,column=0)
 
     #shows the lefthand text box
     def show_me(self):
-        #self.output.pack(side=tk.LEFT)
-        self.output.grid(row=1, column=0)
-        #self.output_display_button.pack_forget()
+        self.output.pack(side=tk.LEFT)
         self.output_display_button.grid_forget()
-        #self.output_hide_button.pack(side=tk.BOTTOM)
-        self.output_hide_button.grid(row=2,column=2)
+        self.output_hide_button.grid(row=0,column=0)
 
     #Close app
     def quit(self, event):
