23e0d935035102e7a1521c77ddc8d6642f8620c8
hcoffey1 <hcoffey1@vols.utk.edu>
2018-09-27 12:02:56 -0400

    Added button to switch to dark theme #19

--- gui.py
+++ gui.py
@@ -34,9 +34,13 @@ class app(object):
         #e.bind('<Control-f>', e.focus) <- Currently not working
         e.grid(row=1, column=0)
 
-        #Light-Dark Button
+        #Light Button
         self.light_b = tk.Button(self.root, text="light", command=self.light)
         self.light_b.pack(side=tk.RIGHT)
+
+        #Dark Button
+        self.dark_b = tk.Button(self.root, text="dark", command=self.dark)
+
 
 
     #Replace contents of text window with data
@@ -72,12 +76,24 @@ class app(object):
         self.output.pack(side=tk.LEFT)
         self.output_display_button.grid_remove()
         self.output_hide_button.grid(row=0,column=0)
-
+    
+    #Set gui to light theme
     def light(self):
         self.root.config(bg="white")
         self.app_name.config(bg="white", fg="black")
         self.prompt_frame.config(bg="white")
         self.output.config(bg="white",fg="black")
+        self.light_b.pack_forget()
+        self.dark_b.pack(side=tk.RIGHT)
+
+    #Set gui to dark theme
+    def dark(self):
+        self.root.config(bg="black")
+        self.app_name.config(bg="black", fg="white")
+        self.prompt_frame.config(bg="black")
+        self.output.config(bg="black",fg="white")
+        self.dark_b.pack_forget()
+        self.light_b.pack(side=tk.RIGHT)
 
     #Close app
     def quit(self, event):
