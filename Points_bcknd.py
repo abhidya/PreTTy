e7831c713e0b5187fd37f1bba49c87e66b73c831
Nathan Johnston <njohns67@vols.utk.edu>
2018-09-29 14:23:20 -0500

    Returns dictionary keyed on file name with points for the value. Fix #26

---
+++ Points_bcknd.py
@@ -0,0 +1,22 @@
+import pickle
+import os, time, math, sys
+
+def parsePickle():
+	filename = "freq_dict.pkl"
+	pkl_file = open(filename, 'rb')
+	files = pickle.load(pkl_file)
+	pkl_file.close()
+	points_dict = {}
+	minTime = sys.maxsize
+	for file in files: 
+		files[file] = os.path.getatime(file)
+		if files[file] < minTime:
+			minTime = files[file]
+	for file, points in files.items():
+		pointValue = math.ceil((points-minTime)/2618000)
+		print(file + ":  " + str(pointValue))
+		points_dict[file] = pointValue
+	return points_dict
+
+
+
