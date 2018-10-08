38f21a7cbffa63d7f5f5b3101555d05008e98173
Manny Bhidya <abhidya@vols.utk.edu>
2018-10-08 16:36:20 -0400

    Added Icons and code icongetter.py for returning icons matching the right extensions!

---
+++ icongetter.py
@@ -0,0 +1,37 @@
+import os.path
+
+
+def extension(filename):
+    # trick for py2/3 compatibility
+    if 'basestring' not in globals():
+        basestring = str
+
+    if isinstance(filename, basestring):
+
+        if os.path.isdir(filename):
+            return "icons/dir.png"
+        else:
+            iconpath = "icons/" + os.path.splitext(filename)[1][1:] + ".png"
+            if os.path.isfile(iconpath):
+                return iconpath
+            else:
+                return 'icons/_blank.png'
+
+    elif all(isinstance(item, basestring) for item in filename):
+        list = []
+        for item in filename:
+            if os.path.isdir(item):
+                list.append("icons/dir.png")
+            else:
+                iconpath = "icons/" + os.path.splitext(item)[1][1:] + ".png"
+                if os.path.isfile(iconpath):
+                    list.append(iconpath)
+                else:
+                    list.append("icons/_blank.png")
+        return list
+
+    else:
+        raise TypeError  # or something along that line
+
+
+print(extension('/home/manny/Desktop/Redacted_Transcript.pdf'))
