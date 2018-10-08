712ee2ac0d8cdb78cd4bcf2b1298dfce9cf241a8
hcoffey1 <hcoffey1@vols.utk.edu>
2018-10-08 02:18:16 -0400

    Added sample function calls for backend use #17

--- theFace.py
+++ theFace.py
@@ -6,4 +6,15 @@ from gui import app
 if __name__ == "__main__":
     root = tk.Tk()
     gui = app(root)
+
+    #Print given text to specified window
+    #gui.update_text(gui.left_window,"Hello world!")
+    #gui.update_text(gui.right_window,"Hello world!")
+
+    #Clear text from window
+    #gui.clear_text(gui.left_window)
+
+    #Append text to window
+    #gui.append_text(gui.left_window, "Extra text!")
+
     root.mainloop()
