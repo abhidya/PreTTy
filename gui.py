c14a5447f041ea925d562d6c8a9a063a47068394
hcoffey1 <hcoffey1@vols.utk.edu>
2018-10-17 16:25:22 -0400

    #42

--- gui.py
+++ gui.py
@@ -27,7 +27,7 @@ class app(object):
         self.prompt_frame.pack(side=tk.BOTTOM)
 
         #Application title displayed on window
-        self.app_logo = tk.PhotoImage(file="logo_light.gif")
+        self.app_logo = tk.PhotoImage(file="graphics/logo_light.gif")
         self.app_logo = self.app_logo.subsample(2, 2)
         self.app_name = tk.Label(self.root, image=self.app_logo, bg=bg_c, fg=fg_c)
         self.app_name.pack(side=tk.TOP)
@@ -103,7 +103,7 @@ class app(object):
             self.right_window.config(bg="black",fg="white")
             #Application title displayed on window
             self.app_name.pack_forget()
-            self.app_logo = tk.PhotoImage(file="logo_light.gif")
+            self.app_logo = tk.PhotoImage(file="graphics/logo_light.gif")
             self.app_logo = self.app_logo.subsample(2, 2)
             self.app_name = tk.Label(self.root, image=self.app_logo, bg="black")
             self.app_name.pack(side=tk.TOP)
@@ -116,7 +116,7 @@ class app(object):
             self.right_window.config(bg="white",fg="black")
             #Application title displayed on window
             self.app_name.pack_forget()
-            self.app_logo = tk.PhotoImage(file="logo.gif")
+            self.app_logo = tk.PhotoImage(file="graphics/logo.gif")
             self.app_logo = self.app_logo.subsample(2, 2)
             self.app_name = tk.Label(self.root, image=self.app_logo, bg="white")
             self.app_name.pack(side=tk.TOP)
