4c30774a4cba08cd5809aebcf6922aedae996f39
Cara Scott <cscott57@vols.utk.edu>
2018-11-07 18:07:15 -0500

    Added theme switch button #58

--- gui.py
+++ gui.py
@@ -115,43 +115,48 @@ class app(object):
         #Window for help
         self.middle_window = tk.Text(self.root, width=40, height=60, wrap=tk.WORD, bg=bg_c, fg=fg_c)
 
+        #BUTTONS
+
         #Buttons to hide and show text display
         self.left_display_button = tk.Button(self.prompt_frame, text="File View", command=self.toggle_left)
         self.left_display_button.grid(row=0, column=0)
 
-        #Buttons to hide and show Graveyard display
-        #self.right_display_button = tk.Button(self.prompt_frame, text = "Graveyard", command = self.toggle_right)
-        #self.right_display_button.grid(row = 0, column = 3)
-
         # load Gravestone image and resize it
-        #self.gravestone_button = tk.PhotoImage(file="graphics/Gravestone.gif")
-        #self.gravestone_button = self.gravestone_button.subsample(2, 2)
-
-        # label with image
-        #l = tk.Label(self.root, image=self.gravestone_button)
-        #l.grid(row = 0, column = 1)
-
-
         self.gravestonepng = tk.PhotoImage(file="graphics/Gravestone.gif")
-        self.gravestonepng = self.gravestonepng.subsample(20, 20)
+        self.gravestonepng = self.gravestonepng.subsample(19, 19)
 
+        #label the button
         self.grave_stone = tk.Label(self.prompt_frame, image=self.gravestonepng, bg=bg_c, fg=fg_c)
 
-        #self.grave_stone.pack(side=tk.RIGHT)
+        #grid the photo as the button
         self.grave_stone.grid(row=0, column=3)
-
+        
+        # bind click event to image
         self.grave_stone.bind("<Button-1>", self.toggle_right)
 
-        # bind click event to image
-        #l.bind('<Button-1>', self.on_click)
-
-        # button with image binded to the same function 
-        #self.right_display_button = tk.Button(self.root, image=self.gravestone_button, command=self.on_click)
+        #Help window toggle button
+        self.help_button = tk.Button(self.prompt_frame, text="Help", command=self.toggle_middle)
+        self.help_button.grid(row = 0, column = 4)
+     
+        #Light / Dark theme toggle button
+        #self.theme_button = tk.Button(
+        #    self.prompt_frame, text="Theme", command=self.theme_toggle)
+        #self.theme_button.grid(row = 0, column = 1)        
         
-        #grid the photo as the button
-        #self.right_display_button.grid(row = 0, column = 1)
+        #load light and dark theme toggle button
+        self.themepng = tk.PhotoImage(file="graphics/theme_button.gif")
+        self.themepng = self.themepng.subsample(2, 2)
 
-        #end of attempt ******
+        #label the button
+        self.theme_switch = tk.Label(self.prompt_frame, image=self.themepng, bg=bg_c, fg=fg_c)
+
+        #grid the photo as the button
+        self.theme_switch.grid(row=0, column=1)
+        
+        # bind click event to image
+        self.theme_switch.bind("<Button-1>", self.theme_toggle)
+
+        #end of buttons ******
 
         #Command prompt
         #TODO: Add hotkey to set focus easily
@@ -159,15 +164,6 @@ class app(object):
         e.focus()
         e.bind('<Return>', self.get)
         e.grid(row=0, column=2)
-
-        #Light / Dark theme toggle button
-        self.theme_button = tk.Button(
-            self.prompt_frame, text="Theme", command=self.theme_toggle)
-        self.theme_button.grid(row = 0, column = 1)
-
-        #Help window toggle button
-        self.help_button = tk.Button(self.prompt_frame, text="Help", command=self.toggle_middle)
-        self.help_button.grid(row = 0, column = 4)
 
     #Replace contents of text window with data
     def update_text(self, window, data):
@@ -232,7 +228,7 @@ class app(object):
 
     #TODO: Optimize this so function does not become too big
     #Toggle between light and dark themes
-    def theme_toggle(self):
+    def theme_toggle(self, event=''):
         if(self.theme_bool):
 
             #dark theme
