28f4dfdcdefb92fbc055db2aa82745e42f7f9606
Nathan Johnston <nathan.johnston359@gmail.com>
2018-11-07 21:16:16 -0500

    Fixed #68

--- Graveyard.py
+++ Graveyard.py
@@ -1,7 +1,19 @@
 import os, time
 import balls, SizeScaler
 
-#Returns 1 if the file belongs int he graveyard
+timeframe = 35400000
+
+def initGraveyard(time, type):
+    if type == "day":
+        timeframe = time * 3600
+    elif type == "week":
+        timeframe = time * 604800
+    elif type == "month":
+        timeframe = time * (604800 * 4)
+    elif type == "year":
+        timeframe = time * 35400000
+
+#Returns 1 if the file belongs in the graveyard
 def checkTime(fileName):
     try:
         if os.path.getatime(fileName) < (time.time() - (604800 * 16)): #35400000 seconds in a year,  604800 seconds in a week, currently set to ~4 months
